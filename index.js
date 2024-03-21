4acff1c413545945024a502652f327183a1e91b3
FrancWash <fvilela216@gmail.com>
2024-03-21 15:29:01 -0300

    Criando o ataque e interpolarizando a string

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+//declarando uma estrutura de classe com variaveis this
 class hero{
-    constructor(nome, idade, tipo){
+    constructor(nome, idade, tipo, atake){
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
+        this.atake = atake
+    }
+ //metodo atacar   
+    atacar(){
+        console.log(`O ${this.tipo} atacou ${this.atake}`)
     }
 }
+
+let heroi = new hero("Tob", 26, "Guerreiro", "usando espada")
+heroi.atacar()
