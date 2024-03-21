fbe12839335d9276221f48880a1658fbc4684e00
FrancWash <fvilela216@gmail.com>
2024-03-21 16:05:05 -0300

    interpolarizando novo herói com seu ataque

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ class hero{
  //metodo atacar   
     atacar(){
         console.log(`O ${this.tipo} atacou ${this.atake}`)
-    }
+    } 
 }
-
+//interpolarização de strings
 let heroi = new hero("Tob", 26, "Guerreiro", "usando espada")
+let heroiNinja = new hero("Naruto", 16, "ninja", "usando shuriken")
 heroi.atacar()
+heroiNinja.atacar()
